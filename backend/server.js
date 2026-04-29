f5f4720791bd4e77e18e4cb78e802e0ac359e9ca
IT23219434 <IT23219434@my.sliit.lk>
2026-04-29 12:36:27 +0530

    changed DB URL

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,10 @@ app.use("/api/items", itemRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => {
